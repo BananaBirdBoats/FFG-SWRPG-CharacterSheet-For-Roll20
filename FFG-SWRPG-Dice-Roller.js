ec4f30db5d7474eb15ad726c78a27d4f5c761727
BananaBirdBoats <ilfrancis60@gmail.com>
2015-10-31 16:13:54 -0400

    Update FFG-SWRPG-Dice-Roller.js
    I would always get the error, "No Destiny Points Defined.  GM needs to add points then update players." The workaround for this was to manually go into your GM dice pool and increase/decrease the pool until the error went away. I made a fix so that you no longer need to do that workaround.

--- FFG-SWRPG-Dice-Roller.js
+++ FFG-SWRPG-Dice-Roller.js
@@ -73,7 +73,6 @@ if (!Date.now) {
       Critical Descriptions by: Gribble - https://dl.dropboxusercontent.com/u/9077657/SW-EotE-Reference-Sheets.pdf
   
   API Chat Commands
-
   Settings:
       Log
           * default: 'on' and 'single'
@@ -136,7 +135,6 @@ if (!Date.now) {
           * default:
           * Description: Rolls 1w die and adds the result to the destiny pool
           * Command: !eed #w destiny doRoll
-
    Other:
       charsheet
           * default:
@@ -145,7 +143,6 @@ if (!Date.now) {
    
    Mechanic Rollers:
    For the following commands, you must first highlight tokens in the play area assigned to characters.
-
       obligation-check
           * default:
           * Description: This allows for an roll for the Obligation against the targets selected.
@@ -314,6 +311,18 @@ eote.createGMDicePool = function () {
             current: 2,
             max: '',
             update: true
+        },
+         {
+            name: 'lightSidePoints',
+            current: 0,
+            max: '',
+            update: true
+        },
+         {
+            name: 'darkSidePoints',
+            current: 0,
+            max: '',
+            update: true
         }
     ];
 
@@ -391,7 +400,8 @@ eote.updateListeners = function (attributes) {
     //Update GM
     var GMObj = findObjs({
         _type: "character",
-        _id: eote.defaults['-DicePoolID']
+       // _id: eote.defaults['-DicePoolID']
+       id: eote.defaults['-GM Command Center-ID']
     });
     eote.updateAddAttribute(GMObj, attributes);
 }
@@ -878,7 +888,7 @@ eote.process.rollPlayer = function (cmd, diceObj) {
 
 eote.process.destiny = function (cmd, diceObj) {
 
-    var charObj_DicePool = findObjs({ _type: "character", name: "-DicePool" })[0];
+    var charObj_DicePool = findObjs({ _type: "character", name: "-GM Command Center-" })[0];
 
     var doRoll = false;
 
